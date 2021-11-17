773a95ce3d7713ae9884cf4cba1e8bbeb7a61cb5
Kenny Nguyen <knguyen111601@gmail.com>
2021-11-17 12:28:19 -0800

    auth added to routes and tested

--- controllers/wishlist.js
+++ controllers/wishlist.js
@@ -4,6 +4,7 @@
 const express = require("express") // express for the router
 const Wishlist = require("../models/wishlist") // wishlist model
 const auth = require("../auth")
+// const User = require("../models/user")
 // THE WAY THIS WORKS IS BY PUTTING AUTH BEFORE (REQ,RES) IN EVERY ROUTE
 // DO THIS LATER AFTER WE GOT THE SITE ALL WORKING
 ///////////////////////////////////////////////////////
@@ -16,34 +17,40 @@ const router = express.Router()
 ///////////////////////////////////////////////////////
 
 // Index Route - get route
-router.get("/wishlist", async (req,res)=>{
+router.get("/wishlist", auth, async (req,res)=>{
     try {
-        res.json(await Wishlist.find({}))
+        const {username} = req.payload
+        res.status(200).json(await Wishlist.find({username}))
     } catch (error) {
         res.status(400).json(error)
     }
 })
 
 // Create Route - post request
-router.post("/wishlist", async (req, res)=>{
+router.post("/wishlist", auth, async (req, res)=>{
     try {
-        res.json(await Wishlist.create(req.body))
+        const {username} = req.payload
+        // the user doesn't write in the username so we make it happen for them
+        req.body.username = username
+        res.status(200).json(await Wishlist.create(req.body))
     } catch (error) {
         res.status(400).json(error)
     }
 })
 
 // Update Route - put request
-router.put("/wishlist/:id", async (req,res)=>{
+router.put("/wishlist/:id", auth, async (req,res)=>{
     try {
-        res.json(await Wishlist.findByIdAndUpdate(req.params.id, req.body, {new: true}))
+        const {username} = req.payload
+        req.body.username = username
+        res.status(200).json(await Wishlist.findByIdAndUpdate(req.params.id, req.body, {new: true}))
     } catch (error) {
         res.status(400).json(error)
     }
 })
 
 // Delete Route - delete request
-router.delete("/wishlist/:id", async (req, res)=>{
+router.delete("/wishlist/:id", auth, async (req, res)=>{
     try {
         res.json(await Wishlist.findByIdAndRemove(req.params.id))
     } catch (error) {
